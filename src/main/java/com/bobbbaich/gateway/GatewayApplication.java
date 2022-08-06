ff2615332d2d42d5b525e2e49976931dc89fa149
bohdanpohorilyi <bohdan.pohorilyi@base.ai>
2022-08-06 17:14:37 +0300

    update: read applicateion

--- src/main/java/com/bobbbaich/gateway/GatewayApplication.java
+++ src/main/java/com/bobbbaich/gateway/GatewayApplication.java
@@ -1,12 +1,34 @@
 package com.bobbbaich.gateway;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+@Slf4j
 @SpringBootApplication
 public class GatewayApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String command = "lsof -i :80";
+
+        Process proc = Runtime.getRuntime().exec(command);
+
+        // Read the output
+
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(proc.getInputStream()));
+
+        String line = "";
+        while((line = reader.readLine()) != null) {
+            System.out.print(line + "\n");
+        }
+
+        proc.waitFor();
+
         SpringApplication.run(GatewayApplication.class, args);
     }
 
